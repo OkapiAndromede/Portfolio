ec8912c1a514b4914e5d4c4c2584d7a83e5dc94f
Neal Muller <neal.henri.muller@gmail.com>
2026-01-21 16:35:43 +0100

    chore : initializing dev branch with local repository

---
+++ src/components/layout/navBar/NavBar.tsx
@@ -0,0 +1,19 @@
+function NavBar() {
+  return (
+    <>
+      <div className="navBar">
+        <ul className="navbar__link">
+          <li className="navbar__link--work">
+            <a href="#">Work</a>
+          </li>
+          <li className="navbar__link--expertise">
+            <a href="#">Expertise</a>
+          </li>
+          <li className="navbar__link--about">
+            <a href="#"> About</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
