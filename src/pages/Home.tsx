1cdb86a562ab77a22a11a0c10ea63b7bb2df0b38
Neal Muller <neal.henri.muller@gmail.com>
2026-01-23 18:28:50 +0100

    feat : add Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/layout/navBar/NavBar";
+
+function Home() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+    </>
+  );
+}
+
+export default Home;
