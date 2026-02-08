137123cc34158e5bda51d07b35bbcb536e6d9303
Neal Muller <neal.henri.muller@gmail.com>
2026-02-08 15:22:22 +0100

    feat (Breaking change) : refactorisation of project data structure

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -1,24 +1,13 @@
-import bookiActiviteDesktop from "../images/booki_activites_desktop.webp";
-import bookiActiviteIpad from "../images/booki_activites_ipad.webp";
-import bookiActiviteMobile from "../images/booki_activites_mobile.webp";
-
-import bookiHebergementDesktop from "../images/booki_hebergement_desktop.webp";
-import bookiHebergementIpad from "../images/booki_hebergement_ipad.webp";
-import bookiHebergementMobile from "../images/booki_hebergement_mobile.webp";
+import bookiActiviteOpti from "../images/booki_activite_opti.webp";
+import bookiHebergementOpti from "../images/booki_hebergement_opti.webp";
 import bookiVideo from "../videos/booki_video.mp4";
 
-import foodMenuDesktop from "../images/food_menu_desktop.webp";
-import foodMenuIpad from "../images/food_menu_ipad.webp";
-import foodMenuMobile from "../images/food_menu_mobile.webp";
-import foodOrder from "../images/food_commande.png";
-
+import foodPrincipleOpti from "../images/food_principle_opti.webp";
+import foodRestaurantOpti from "../images/food_menu_opti.webp";
 import foodVideo from "../videos/food_video.mp4";
 
-import kasaHouseDesktop from "../images/kasa_appartement_desktop.webp";
-import kasaHouseIpad from "../images/kasa_appartement_ipad.webp";
-import kasaHouseMobile from "../images/kasa_appartement_mobile.webp";
-
-import kasaExplain from "../images/kasa_explication.png";
+import kasaHouseOpti from "../images/kasa_house_opti.webp";
+import kasaHouse2Opti from "../images/kasa_house2_opti.webp";
 import kasaVideo from "../videos/kasa_video.mp4";
 
 export type Project = {
@@ -26,16 +15,8 @@ export type Project = {
   number: string;
   title: string;
   images: {
-    first: {
-      small: string;
-      medium: string;
-      large: string;
-    };
-    second: {
-      small: string;
-      medium: string;
-      large: string;
-    };
+    first: string;
+    second: string;
   };
   video: string;
 };
@@ -45,16 +26,8 @@ export const projects: Project[] = [
     number: "001",
     title: "Booki",
     images: {
-      first: {
-        small: bookiActiviteMobile,
-        medium: bookiActiviteIpad,
-        large: bookiActiviteDesktop,
-      },
-      second: {
-        small: bookiHebergementMobile,
-        medium: bookiHebergementIpad,
-        large: bookiHebergementDesktop,
-      },
+      first: bookiActiviteOpti,
+      second: bookiHebergementOpti,
     },
     video: bookiVideo,
   },
@@ -63,16 +36,8 @@ export const projects: Project[] = [
     number: "002",
     title: "Oh my food",
     images: {
-      first: {
-        small: foodMenuMobile,
-        medium: foodMenuIpad,
-        large: foodMenuDesktop,
-      },
-      second: {
-        small: foodOrder,
-        medium: foodOrder,
-        large: foodOrder,
-      },
+      first: foodPrincipleOpti,
+      second: foodRestaurantOpti,
     },
     video: foodVideo,
   },
@@ -81,16 +46,8 @@ export const projects: Project[] = [
     number: "003",
     title: "Kasa",
     images: {
-      first: {
-        small: kasaHouseMobile,
-        medium: kasaHouseIpad,
-        large: kasaHouseDesktop,
-      },
-      second: {
-        small: kasaExplain,
-        medium: kasaExplain,
-        large: kasaExplain,
-      },
+      first: kasaHouseOpti,
+      second: kasaHouse2Opti,
     },
     video: kasaVideo,
   },
