df13b1d51195cc13e3444114db92885152044546
Neal Muller <neal.henri.muller@gmail.com>
2026-02-13 14:06:58 +0100

    feat : add files of contact page

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,13 @@
+import style from "./Contact.module.scss";
+
+function Contact() {
+  return (
+    <>
+      <main>
+        <div className={style["contactBg"]}> </div>
+      </main>
+    </>
+  );
+}
+
+export default Contact;
