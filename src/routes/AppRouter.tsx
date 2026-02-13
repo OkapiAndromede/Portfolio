8d94af6f1551751065ad490954a827470e7fe33b
Neal Muller <neal.henri.muller@gmail.com>
2026-02-13 14:08:47 +0100

    feat (contact route) : add contact route on the router

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
+import Contact from "../pages/contact/Contact";
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
         <Route path="*" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
