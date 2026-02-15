80d89197dd1c2fbb328ee23672bf38f4e25398d5
Neal Muller <neal.henri.muller@gmail.com>
2026-02-15 12:22:29 +0100

    feat (HomeButton) : add HomeButton component and link to home page

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+import { HomeButton } from "../../components/UI/button/Button";
 import Form from "../../components/UI/form/Form";
 import style from "./Contact.module.scss";
 
@@ -5,7 +7,11 @@ function Contact() {
   return (
     <>
       <main className={style["contactLayout"]}>
-        <div className={style["contactLayout__bg"]}> </div>
+        <div className={style["contactLayout__bg"]}>
+          <NavLink to={"/"}>
+            <HomeButton className={style["contactLayout__bg--button"]} />
+          </NavLink>
+        </div>
         <div className={style["contactLayout__form"]}>
           <Form />
         </div>
