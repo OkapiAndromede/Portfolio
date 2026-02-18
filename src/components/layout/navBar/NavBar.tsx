d7a0dbdc451d60dfc219c52ef41f03b77d24ce2c
Neal Muller <neal.henri.muller@gmail.com>
2026-02-18 11:21:17 +0100

    feat : activate the anchor

--- src/components/layout/navBar/NavBar.tsx
+++ src/components/layout/navBar/NavBar.tsx
@@ -6,13 +6,13 @@ function NavBar() {
       <div className={styles["navBar"]}>
         <ul className={styles["navbar__link"]}>
           <li className={styles["navbar__link--work"]}>
-            <a href="#">Work</a>
+            <a href="#projectSection">Work</a>
           </li>
           <li className={styles["navbar__link--expertise"]}>
-            <a href="#">Expertise</a>
+            <a href="#expertiseSection">Expertise</a>
           </li>
           <li className={styles["navbar__link--about"]}>
-            <a href="#"> About</a>
+            <a href="#aboutSection"> About</a>
           </li>
         </ul>
       </div>
